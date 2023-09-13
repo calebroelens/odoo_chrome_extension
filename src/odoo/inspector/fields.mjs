6d45c397399da68de31853b7fafd618ecab25b00
calebroelens <calebroelens@hotmail.com>
2023-09-14 00:15:15 +0200

    [ADD] Progress on field detector

--- src/odoo/inspector/fields.mjs
+++ src/odoo/inspector/fields.mjs
@@ -24,23 +24,68 @@ let checkForField = (contextEventData, clickData) => {
                 }
                 return {
                     type: 'field',
+                    variant: null,
                     final_target: target,
                     data: {
-                        href: field_data_element.href,
                         field_name: field_data_element.name,
                         text_name: target.innerText,
                         field_value: field_value
                     }
                 }
             }
-        }
-    }
-    // Case 2: Clicked TD tag (V15)
-    if(target.tagName && target.tagName === "TD" && target.className.includes("o_td_label")){
+        } else {
+            // Wrapped in div
+            if(target.parentElement.tagName === "DIV" && target.parentElement.className.includes('o_wrap_label')){
+                let field_link_name = target.getAttribute('for');
+                let field_data_element = document.getElementById(field_link_name);
+                let field_value = null;
+                if(field_data_element){
+                    field_value = field_data_element.value;
+                } else {
+                    field_data_element = document.querySelector(`div[name='${field_link_name}'].o_readonly_modifier`);
+                    if(field_data_element){
+                        field_value = field_data_element.firstElementChild.value;
+                    }
+                }
+                if(field_data_element && field_data_element.dataset.field){
+                    field_link_name = field_data_element.dataset.field;
+                }
+                // Refactor this when I'm not lazy
+                if(field_data_element.parentElement.className.includes('dropdown')){
+                    if(field_data_element.parentElement.parentElement.className.includes('dropdown')){
+                        if(field_data_element.parentElement.parentElement.parentElement.parentElement.getAttribute('name')){
+                            field_link_name = field_data_element.parentElement.parentElement.parentElement.parentElement.getAttribute('name')
+                        }
+                    }
+                }
+                if(field_data_element.parentElement.className.includes('o_field_widget')){
+                    if(field_data_element.parentElement.getAttribute('name')){
+                        field_link_name = field_data_element.parentElement.getAttribute('name')
+                    }
+                }
+                if(field_data_element.id && field_data_element.id.includes('_0')){
+                    if(field_data_element.parentElement.tagName  === "DIV" && field_data_element.parentElement.className.includes('text')){
+                        if(field_data_element.parentElement.parentElement.tagName === "DIV"
+                            && field_data_element.parentElement.parentElement.getAttribute('name')){
+                            field_link_name = field_data_element.parentElement.parentElement.getAttribute('name');
+                        }
+                    }
+                }
+                return {
+                    type: 'field',
+                    variant: null,
+                    final_target: target,
+                    data: {
+                        field_name: field_link_name,
+                        text_name: target.innerText,
+                        field_value: field_value
+                    }
+                }
 
-    }
-    // Case 3: Div surrounding label
-    if(target.tagName && target.tagName === "DIV" && target.className.includes("o_wrap_label")){
+            }
+        }
+    } else if (target.tagName === "SELECT" && target.id){
+        // Might be a select field input
 
     }
 
